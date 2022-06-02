bf5a36ec7910f41ff8d72b38c614ba5be7793d97
Saman <saman@DESKTOP-0HI886Q>
2022-06-02 13:06:08 -0400

    Implementing entity classes

---
+++ src/com/myCompany/thrillio/entities/Book.java
@@ -0,0 +1,10 @@
+package com.myCompany.thrillio.entities;
+
+public class Book extends Bookmark {
+	private int publicationYear;
+	private String publisher;
+	private String[] authors;
+	private String genre;
+	private double amazonRating;
+	
+}
---
+++ src/com/myCompany/thrillio/entities/webLink.java
@@ -0,0 +1,23 @@
+package com.myCompany.thrillio.entities;
+
+public class webLink extends Bookmark {
+	private String url;
+	private String host;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+}
